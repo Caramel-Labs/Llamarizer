01803b3f920ed8e6e99449abb9bd4435fc96a83c
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-08-27 11:33:45 +0530

    Created text summarizer

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import llama
 
-st.title("Echo Bot")
+st.title("Llamarizer")
+
+print(st.session_state)
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -12,16 +14,25 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+print(st.session_state.messages)
+
 # React to user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Enter text to summarize"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-response = f"Echo: {prompt}"
-response = llama.get_response(prompt)
+print(st.session_state.messages)
+
+modified_prompt = ""
+response = ""
+
+if prompt:
+    modified_prompt = f"""Summarize the following text: \n{prompt} \n\nSummary: """
+    print(modified_prompt)
+    response = llama.get_response(modified_prompt)
 
 # Display assistant response in chat message container
 with st.chat_message("assistant"):
@@ -29,3 +40,5 @@ with st.chat_message("assistant"):
 
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
+
+print(st.session_state.messages)
